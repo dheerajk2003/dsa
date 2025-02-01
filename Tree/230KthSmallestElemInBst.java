8c9f5efab67437c6e7485eb2e0c0f3cb1e3e52e4
dheerajk2003 <dheerajkhatri2003@gmail.com>
2025-02-01 17:58:23 +0530

    modified 230

--- Tree/230KthSmallestElemInBst.java
+++ Tree/230KthSmallestElemInBst.java
@@ -3,15 +3,14 @@ class Solution {
     public void els(TreeNode node){
         if(node == null)
             return;
-        ls.add(node.val);
         els(node.left);
+        ls.add(node.val);
         els(node.right);
     }
     public int kthSmallest(TreeNode root, int k) {
         els(root);
-        Collections.sort(ls);
         return ls.get(k-1);
     }
 }
 
-// this we will do using an arrray and sort that array
+// this we will traverse the tree in infix order.
