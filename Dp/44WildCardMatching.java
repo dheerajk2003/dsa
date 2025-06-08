7b18db99272c24e50cc1ba00382e7e3074a40da9
Dheeraj Khatri <96782450+dheerajk2003@users.noreply.github.com>
2025-06-08 13:26:12 +0530

    Update 44WildCardMatching.java tabulization solution

--- Dp/44WildCardMatching.java
+++ Dp/44WildCardMatching.java
@@ -35,3 +35,53 @@ class Solution {
         return helper(s, p, 0, 0);
     }
 }
+
+
+
+// or more optimal solution using tabulization
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+       int slen = s.length();
+       int plen = p.length();
+       if(slen == 0 && plen == 0) 
+            return true;
+        if(plen == 0)
+            return false;
+        if(slen == 0){
+            for(int i=0; i<plen; i++){
+                if(p.charAt(i) != '*')
+                    return false;
+            }
+            return true;
+        }
+        
+
+        boolean dp[][] = new boolean[slen+1][plen+1];
+        dp[slen][plen] = true;
+
+        // for handling * at end of p
+        for (int j = plen - 1; j >= 0; j--) {
+            if (p.charAt(j) == '*') {
+                dp[slen][j] = dp[slen][j + 1];
+            } else {
+                break;
+            }
+        }
+
+        for(int i=slen-1; i>=0; i--){
+            for(int j=plen-1; j>=0; j--){
+                if((s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') && (dp[i+1][j+1])){
+                    dp[i][j] = true;
+                }
+                else if(p.charAt(j) == '*' && (dp[i][j+1] || dp[i+1][j])){
+                    dp[i][j] = true;
+                }
+                else
+                    dp[i][j] = false;
+            }
+        }
+
+        return dp[0][0];
+    }
+}
